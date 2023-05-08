1b4289b5bbdbfbbd785338234887cfcaedda8a12
M-Metaw3 <mizoomizoo161@gmail.com>
2023-05-08 19:05:15 +0200

    the final

--- dhad-academy/src/components/Mylearning/Mylearning.jsx
+++ dhad-academy/src/components/Mylearning/Mylearning.jsx
@@ -40,38 +40,35 @@ console.log(lessons.lessons);
     
     if (course?.lessons.length>0) {
       
-      nav(`/course/${course.courseName}`)
+      nav(`/course/${course.title}`)
     }
     else{
         toast.success('no lessons in this course yet '); // Display a success message using toast     
     }
   }
   return (
-    <Container className='py-5' >
+    <Container className='py-5'>
     <div className="row d-flex justify-content-center">
       <ToastContainer />
-      {coursesUsers && coursesUsers.length > 0 && coursesUsers.some(course => course.courseId) ? (
-    coursesUsers.map((course) => (
-        // add condition to check for null course ID
-       course.courseId && (
-            <div className="col-10 col-md-6 col-lg-4 pb-5">
-                <div className="card rounded-20">
-                    <img src={`${apihttp}${course.courseId?.image}`} height={250} className="rounded-img-top" alt="..." />
-                    <div className="card-body d-flex flex-column justify-content-between">
-                        <p className="card-title"><i className="fa-regular fa-clock pe-1"></i>{course.courseId?.hours} {t('Hours')}</p>
-                        <h6 className="card-text">{course.courseId?.courseName } </h6>
-                        <h6 className="card-text">{course.courseId?.lessons.length} {t("Lessons")} </h6>
-                        <button onClick={()=>handellersendLessons(course.courseId)} className="w-100 btn-submit btn px-5">{t("StartNow")}</button>
-                    </div>
+      {coursesUsers ? coursesUsers.map((course) => (
+    // add condition to check for null course ID
+    course.courseId && (
+        <div className="col-10 col-md-6 col-lg-4 pb-5">
+            <div className="card rounded-20">
+                <img src={`${apihttp}${course.courseId?.image}`} height={250} className="rounded-img-top" alt="..." />
+                <div className="card-body d-flex flex-column justify-content-between" style={{height:"200px"}}>
+                    <p className="card-title"><i className="fa-regular fa-clock pe-1"></i>{course.courseId?.hours} {t('Hours')}</p>
+                    <h6 className="card-text">{course.courseId?.title } </h6>
+                    <h6 className="card-text">{course.courseId?.lessons.length} {t("Lessons")} </h6>
+                    <button onClick={()=>handellersendLessons(course.courseId)} className="w-100 btn-submit btn px-5">{t("StartNow")}</button>
                 </div>
             </div>
-       )
-    ))
-) : (
-    <div className="d-flex justify-content-center align-items-center" >
-        <p>no courses yet</p> 
-    </div>
-)}
+        </div>
+    )
+)) :
+<div className="d-flex justify-content-center align-items-center" style={{height:"200px"}}>
+    <p>no courses yet</p> 
+</div>}
 </div>
     </Container>
     
