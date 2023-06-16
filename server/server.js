9fe9a88473462a2811f4bde5bffe449fdc8b1f7e
nadah79 <nadah.hesham.sh@gmail.com>
2023-06-16 22:06:39 +0300

    update

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const app=require('./utils/src')
 console.log("done")
-const port=process.env.PORT||8000
+const port=process.env.PORT||5000
 
 app.listen(port,()=>{
     console.log(`http://localhost:${port}`)
